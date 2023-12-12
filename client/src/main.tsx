e1e2a0cd801e111b3cb25a91f18768ecc1fd3596
Bilal Sheikh <72bilal.sheikh@gmail.com>
2023-12-12 11:02:22 +0530

    first commit

---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import React from "react";
+import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<NextUIProvider>
+			<App />
+		</NextUIProvider>
+	</React.StrictMode>
+);
