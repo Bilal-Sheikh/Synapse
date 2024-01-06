4d0eb3747f09c06f8b48515895ea4d9f9d85fcb9
Bilal Sheikh <72bilal.sheikh@gmail.com>
2024-01-06 11:46:47 +0530

    added env

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,20 @@ import express from "express";
 import cors from "cors";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const FE_URL = process.env.FE_URL || "http://localhost:5173";
+
 app.use(cors());
 
 const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: FE_URL,
     },
 });
 
